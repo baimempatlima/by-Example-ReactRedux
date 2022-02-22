2ccdcaea490d9e52d7ad5f84a0ec2e7969df3d6f
baimempatlima <baimempatlima@gmail.com>
2022-02-22 20:16:19 +0700

    add task redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import Komponen from "./Materi/Komponen";
+// import Komponen from "./Materi/Komponen";
 
+import Redux from "./Materi/Redux";
+// import Routing from "./Materi/Routing";
+
+// import { Hooks } from "./Materi/Hooks";
+
+// import Rendering from "./Materi/Rendering";
+
+// import Styling from "./Materi/Styling";
+// import Reusable from "./Materi/Reusable/Index";
+// import Form from "./Materi/Form";
 function App() {
   return (
     <div className="App">
-      <Komponen />
+      {/* <Komponen /> */}
+      {/* <Styling /> */}
+      {/* <Reusable /> */}
+      {/* <Rendering /> */}
+      {/* <Form /> */}
+      {/* <Hooks /> */}
+      <Redux />
+      {/* <Routing /> */}
     </div>
   );
 }
